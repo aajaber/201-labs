4cd858288c36249fec0d9fc030c6329f49480f08
aajaber <aajaber.97@gmail.com>
2021-06-20 15:28:35 +0300

    first lab in 201

---
+++ javaScriptFile.js
@@ -0,0 +1,22 @@
+let first_Question =prompt('Welcome to 201 first Lab,Are you ready ?');
+console.log(first_Question);
+
+let second_Question =prompt('What\'s your name ?');
+console.log(first_Question);
+
+let third_Question =prompt('what\'s your favorite car?');
+console.log(first_Question);
+
+let fourth_Question =prompt('why\'s that?');
+console.log(first_Question);
+
+
+
+
+let favCar;
+favCar="Alfa Romeo";
+console.log(favCar);
+
+
+const link="https://www.google.com/";
+console.long(link);
